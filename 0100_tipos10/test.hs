9ed64ae1a4dbe4f278e5ca94e5e3ba3e6aa06d09
Mariana <mmatos87@gmail.com>
2015-04-24 15:54:48 -0300

    Test del ejecicio 10 arreglado

--- 0100_tipos10/test.hs
+++ 0100_tipos10/test.hs
@@ -1,6 +1,6 @@
 describe "tipos10" $ do
-  it "la expresion f1 (:\"a\") (:\"b\") a == True debería tipar" $ do
-    const True (f1 (:"a") (:"b") a == True) `shouldBe` True
+  it "la expresion f1 (:\"a\") (:\"b\") 'a' == True debería tipar" $ do
+    const True (f1 (:"a") (:"b") 'a' == True) `shouldBe` True
 
   it "la expresion f1 (+1) (*2) 4 == False debería tipar" $ do
     const True (f1 (+1) (*2) 4 == False) `shouldBe` True
